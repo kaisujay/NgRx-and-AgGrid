98ee0a073e4281f4767232707a0e68c698676ed1
kaisujay <kai.sujay@gmail.com>
2024-01-21 12:08:19 +0530

    Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'testing',
+    loadChildren: () => import('./modules/testing/testing.module').then(m => m.TestingModule)
+  }
+];
+
+// For Another "Wrapper"
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule]
+// })
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    useHash: true,
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/modules/testing/testing-routing.module.ts
+++ src/app/modules/testing/testing-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestingComponent } from './component/testing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: TestingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
